4f061b085b13fb2102879e92cab62cb216b8e83e
Geoff Myers <geoff@simdex.org>
2022-08-26 05:42:56 -0500

    Initial commit

---
+++ src/SimDex/SimDexCommon.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SimDex\SimDexCommon;
+
+class Common
+{
+    public static function tableHeaderHTML(array $header_columns): string
+    {
+        $html = '
+        <table>
+            <thead>
+                <tr>
+        ';
+
+        foreach ($header_columns as $class => $header_column) {
+            $html .= '<th class="' . $class . '">' . $header_column . '</th>';
+        }
+
+        $html .= '
+                </tr>
+            </thead>
+            <tbody>
+        ';
+
+        return $html;
+    }
+
+    public static function tableRowHTML(array $columns): string
+    {
+
+        $html = '<tr>';
+
+        foreach ($columns as $class => $column) {
+            $html .= '<td class="' . $class . '">' . $column . '</td>';
+        }
+
+        $html .= '</tr>';
+
+        return $html;
+    }
+
+    public static function tableFooterHTML(array $footer_columns): string
+    {
+        $html = '
+        </tbody>
+        <tfoot>
+            <tr>
+        ';
+
+        foreach ($footer_columns as $class => $footer_column) {
+            $html .= '<th class="' . $class . '">' . $footer_column . '</th>';
+        }
+
+        $html .= '
+                </tr>
+            </tfoot>
+        </table>
+        ';
+
+        return $html;
+    }
+}
