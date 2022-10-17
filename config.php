341daf4a8a97f0379f4089a23dddcb5a6d1681c9
Geoff Myers <geoff@simdex.org>
2022-10-17 12:06:14 -0500

    Added file: config.php; added constant to configuration file: SIMDEX_IPGEOLOCATION_API_KEY

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * SimDex Common
+ * PHP Version 8.1
+ *
+ * @category Common
+ * @package  SimDex\Commnon
+ * @author   Geoff Myers <geoff@simdex.org>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU GPL 3.0
+ * @link     https://github.com/SimDex-LLC/SimDex-Common
+ */
+
+namespace SimDex\Common;
+
+define('SIMDEX_IPGEOLOCATION_API_KEY', 'b5469f65ca6d48ac9cea88d720d4e966');
